a592e0624a7974f1beadb3dcbab66bcf58b10473
mwdjones <jone3247@umn.edu>
2023-09-11 11:43:53 -0500

    init SPRUCE run

---
+++ ELM-SPRUCE/initRUN/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/topology.c" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/topology.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_NETCDF"
+  "_NOPNETCDF"
+  "_NOUSEMCT"
+  "_USEBOX"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio"
+  "pio"
+  )
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/alloc_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/alloc_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/box_rearrange.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/box_rearrange.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/calcdecomp.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/calcdecomp.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/calcdisplace_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/calcdisplace_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/iompi_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/iompi_mod.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/ionf_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/ionf_mod.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/nf_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/nf_mod.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_kinds.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_kinds.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_mpi_utils.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_mpi_utils.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_msg_callbacks.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_msg_callbacks.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pio_msg_getput_callbacks.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_msg_getput_callbacks.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_msg_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_msg_mod.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_nf_utils.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_nf_utils.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pio_spmd_utils.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_spmd_utils.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_support.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_support.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_types.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_types.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/pio_utils.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pio_utils.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/piodarray.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/piodarray.F90.o"
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio/piolib_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/piolib_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pionfatt_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pionfatt_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pionfget_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pionfget_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pionfput_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pionfput_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pionfread_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pionfread_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/pionfwrite_mod.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/pionfwrite_mod.F90.o"
+  "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/rearrange.F90" "/home/mwjones/E3SM-SL/run/TEST_US-SPR_ICB1850CNRDCTCBC_ad_spinup/bld/gnu/mpi-serial/nodebug/nothreads/mct/pio/pio1/pio/CMakeFiles/pio.dir/rearrange.F90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_Fortran
+  "_NETCDF"
+  "_NOPNETCDF"
+  "_NOUSEMCT"
+  "_USEBOX"
+  )
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "/home/mwjones/E3SM-SL/cime/src/externals/pio1/pio"
+  "pio"
+  "/usr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
